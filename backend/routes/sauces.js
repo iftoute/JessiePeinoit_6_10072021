a59cda1c0d02fece821dfaf6c73984c84bab602b
iftoute <peinoit.jessie@orange.fr>
2021-07-24 18:44:42 +0200

    Add multer

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
 const saucesCtrl = require('../controllers/sauces');
 
-router.post('/', saucesCtrl.createSauce);
-router.put('/:id', saucesCtrl.modifySauce);
-router.delete('/:id', saucesCtrl.deleteSauce);
-router.get('/', saucesCtrl.getAllSauces);
-router.get('/:id', saucesCtrl.getOneSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
 
 module.exports = router;
